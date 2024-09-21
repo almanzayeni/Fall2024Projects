4ca4580d5a8145d0bb17a44aa862cda1ef15f16c
Yeni Almanza <ya00013@my.westga.edu>
2024-09-21 08:36:52 -0400

    1A-UI for Adding Food
    Updated the fxml file to include controls for entering a new food,
    choosing the type of new food, adding the new food, and selecting it from
    a list. Also, updated codebehind class to incude the fxml fields.

--- code/FoodPantry/src/edu/westga/cs1302/foodpantry/view/MainWindow.java
+++ code/FoodPantry/src/edu/westga/cs1302/foodpantry/view/MainWindow.java
@@ -1,10 +1,40 @@
 package edu.westga.cs1302.foodpantry.view;
 
-/** Codebehind for the Main Window of the application.
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * The codebehind class for the food pantry
  * 
- * @author CS 1302
+ * @author Yeni Almanza
  * @version Fall 2024
  */
 public class MainWindow {
 
+    @FXML
+    private TextField foodNameField;
+
+    @FXML
+    private ComboBox<?> foodTypeComboBox;
+
+    @FXML
+    private ListView<?> pantryListView;
+
+    @FXML
+    void addFood(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert this.foodNameField != null : "fx:id=\"foodNameField\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.foodTypeComboBox != null : "fx:id=\"foodTypeComboBox\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert this.pantryListView != null : "fx:id=\"pantryListView\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+    }
+
 }
+
