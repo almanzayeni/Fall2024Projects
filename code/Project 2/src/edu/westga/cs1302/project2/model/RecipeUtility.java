8d431bb80bb4c136bc890279d8d71dc5ee80a505
Yeni Almanza <ya00013@my.westga.edu>
2024-10-30 17:12:55 -0400

    3B Update BL to Load Recepies
    Added a method to the uitlity class to convert the list of recepies
    from the loaded file. Implemented a class to load the list of
    recipies from a loaded file and addeda empty line then returned an
    empty list. Also added a methos return a list of recipies with a
    specified ingredient and if not found then returned an empty list.

--- code/Project 2/src/edu/westga/cs1302/project2/model/RecipeUtility.java
+++ code/Project 2/src/edu/westga/cs1302/project2/model/RecipeUtility.java
@@ -1,5 +1,7 @@
 package edu.westga.cs1302.project2.model;
 
+import java.util.List;
+
 /**
  * Utility for formatting recipe object to string
  * 
@@ -24,4 +26,22 @@ public class RecipeUtility {
 		}
 		return recipe.getName() + "\n" + ingredientsList.toString();
 	}
+	
+    /**
+     * Converts a list of recipe objects to a String, separated by blank lines.
+     * 
+     * @param recipes the list of recipes to be converted
+     * @return string representation of the list of recipes
+     */
+    public static String convertRecipesToString(List<Recipe> recipes) {
+        StringBuilder result = new StringBuilder();
+        for (int index = 0; index < recipes.size(); index++) {
+            result.append(converRecipeToString(recipes.get(index)));
+            if (index < recipes.size() - 1) {
+                result.append("\n\n");
+            }
+        }
+        return result.toString();
+    }
+
 }
